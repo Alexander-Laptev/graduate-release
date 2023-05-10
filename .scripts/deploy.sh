b328462b604d44c0053d4dfbb6612aa4d26a8465
Alexandr <sashalaptev2003@gmail.com>
2023-05-10 19:47:53 +0400

    change action

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,17 +3,21 @@ set -e
 
 echo "Deployment started ..."
 
-cd ~/var/www/graduate
+cd ..
+cd var/www/graduate
+
+git pull
 
 docker-compose down
 
 docker exec -it graduate_app bash
+
 # Enter maintenance mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull
+
 
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader

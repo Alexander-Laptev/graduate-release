0a65ecf22ef4481c0b51cb50f4b4dda32a6a5b28
Alexandr <sashalaptev2003@gmail.com>
2023-05-10 21:01:34 +0400

    change action

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,34 +3,7 @@ set -e
 
 echo "Deployment started ..."
 
-git pull
-
-docker-compose -f docker-compose.prod.yml up -d
-
-docker exec -it graduate_app bash
-
-# Enter maintenance mode or return true
-# if already is in maintenance mode
-(php artisan down) || true
-
-# Pull the latest version of the app
-
-
-# Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-
-# Clear the old cache
-php artisan clear-compiled
-
-# Recreate cache
-php artisan optimize
-
-# Run database migrations
-php artisan migrate --force
-
-# Exit maintenance mode
-php artisan up
-
-exit
+git checkout production
+git pull origin production
 
 echo "Deployment finished!"

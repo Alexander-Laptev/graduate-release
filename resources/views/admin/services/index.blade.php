d462eb71103bf6ba01ddda04bda03094d1e74baa
Alexandr <sashalaptev2003@gmail.com>
2023-06-05 02:59:03 +0400

    Формат времени в услугах

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -75,7 +75,7 @@
                             {{ $service->cost }}
                         </td>
                         <td class="px-6 py-4">
-                            {{ $service->time->fotmat('H:i') }}
+                            {{ $service->time->format('H:i') }}
                         </td>
                         <td class="px-6 py-4">
                             {{ $service->description }}

475815a3c7329142d910f0fe0e01ab8c8b89856c
Alexandr <sashalaptev2003@gmail.com>
2023-06-05 02:13:13 +0400

    Редактирование профиля

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,6 +38,7 @@ class RegisteredUserController extends Controller
             'login' => ['required', 'string', 'max:255', 'unique:'.User::class],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'role_id' => ['nullable']
         ]);
 
         $roles = Role::all();

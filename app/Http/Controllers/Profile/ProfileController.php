5bf16427dd70509da5a4623d125d9de7463211d0
Alexandr <sashalaptev2003@gmail.com>
2023-06-07 02:39:19 +0400

    правки

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Profile;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\ProfileUpdateRequest;
 use App\Models\Customer;
 use Illuminate\Http\RedirectResponse;

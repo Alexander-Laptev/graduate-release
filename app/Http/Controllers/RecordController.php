8f02754056247a12057c6a942ff9cbae7b1ddcd3
Alexandr <sashalaptev2003@gmail.com>
2023-06-21 02:30:37 +0400

    Перед защитой

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -283,9 +283,9 @@ class RecordController extends Controller
                 $time = $timesOpen->shift();
                 while($time['timeStart'] <= $time['timeClose'])
                 {
-                    $start = $time['timeStart']->format('H:i:s');
-                    $now = Carbon::now($city->timezone)->format('H:i:s');
-                    if($start >= $now)
+//                    $start = $time['timeStart']->format('H:i:s');
+//                    $now = Carbon::now($city->timezone)->format('H:i:s');
+//                    if($start >= $now)
                         $times->push(['id' => $i, 'hour' =>  $time['timeStart']->format('H'), 'minute' => $time['timeStart']->format('i')]);
                     $time['timeStart']->addMinutes(5);
                 }
